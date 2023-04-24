dcdcc6c45b442cbd01aab9aca5ef70b7c5134a94
tarek95 <tare2sa3eed@gmail.com>
2023-04-24 22:58:31 +0200

    add UI of Result area

--- src/Component/ResultArea.jsx
+++ src/Component/ResultArea.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 function ResultArea() {
   return (
-    <div>ResultArea</div>
+    <div className="result-area">
+      <div className="tip-amount">
+          <div className="text">
+            <div className="head">Tip Amount</div>
+            <p>/ person</p>
+          </div>
+          <div className="tip-result">
+            $0.00
+          </div>
+      </div>
+      <div className="total">
+          <div className="text">
+            <div className="head">Total</div>
+            <p>/ person</p>
+          </div>
+          <div className="total-result">
+            $0.00
+          </div>
+      </div>
+      <button className="reset">
+        Reset
+      </button>
+    </div>
   )
 }
 
