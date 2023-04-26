f8f73ae5ee45452250c5e04aeedb938e1c4f4a9b
tarek95 <tare2sa3eed@gmail.com>
2023-04-26 11:23:38 +0200

    refactor manage area and fix some bugs

--- src/Component/ManageArea.jsx
+++ src/Component/ManageArea.jsx
@@ -1,91 +1,88 @@
 import dollar from "../assets/images/icon-dollar.svg";
 import person from "../assets/images/icon-person.svg";
-import { useEffect, useRef, useState } from "react";
-function ManageArea({
-  setTipAmount,
-  setTotal,
-  isReseted,
-  setIsReseted,
-}) {
-  const [calcManager,setCalcManager] = useState({
-    tip:0,people:null,bill:null
+import { useEffect, useMemo, useState } from "react";
+function ManageArea({ setTipAmount, setTotal, isReseted, setIsReseted }) {
+  const [calcManager, setCalcManager] = useState({
+    tip: 0,
+    people: null,
+    bill: null,
   });
-  const {tip,people,bill}=calcManager
-  const allBtns = document.querySelectorAll(".btn");
-  const CustomTip = useRef();
+  const { tip, people, bill } = calcManager;
   const tips = [5, 10, 15, 20, 25];
+  const [currentTip,setCurrentTip] = useState();
+  const [customTip,setCustomTip] = useState("");
 
   useEffect(() => {
     if (bill > 0 && tip > 0 && people > 0) {
-      setTipAmount(((bill * (tip / 100)) / people));
-      setTotal(((bill * (tip / 100)) / people + bill / people));
+      setTipAmount((bill * (tip / 100)) / people);
+      setTotal((bill * (tip / 100)) / people + bill / people);
     }
   }, [calcManager]);
 
   //check every render if reseted button is clicked
   useEffect(() => {
+    
     if (isReseted) {
-      allBtns.forEach((btn) => btn.classList.remove("choosed"));
-      CustomTip.current.value = null;
+      setCalcManager({ tip: 0, people: null, bill: null });
+      setCurrentTip()
+      setTotal(0);
+      setTipAmount(0);
+      setCustomTip("")
     }
-    if(isReseted){
-      setCalcManager({ tip:0,people:null,bill:null})
-
-    setTotal(0);
-    setTipAmount(0);
-    }
-  },[isReseted]);
-
-  //handle inputs depend on their state name
-  const clearTips = () => {
-    allBtns.forEach((btn) => {
-      btn.classList.remove("choosed");
-    });
-  };
+  }, [isReseted]);
 
   //handle tip buttons if any of them clicked
-  const tipClickHandler = (e) => {
-    setIsReseted(false)
-    clearTips(e);
-    let allBtns = e.currentTarget.parentNode.childNodes;
-    allBtns.forEach((btn) => {
-      btn.classList.remove("choosed");
-    });
-    e.currentTarget.classList.add("choosed");
+  const tipClickHandler = (i) => {
+    setCustomTip("")
+    setIsReseted(false);
+    setCurrentTip(i)
   };
 
+  const customTipHandler=(e)=>{
+    {
+      setCustomTip(e.target.value)
+      setIsReseted(false)
+        setCurrentTip()
+    }
+  }
   //create tip buttons dynamically
-  const tipBtns = tips.map((tip, index) => {
-    let tipNum = "tip" + (index + 1);
-    return (
-      <div key={index} className={`${tipNum} btn `} onClick={tipClickHandler}>
-        <label htmlFor={tipNum}>{tip}%</label>
-        <input type="radio" name="tip" value={tip} id={tipNum} hidden />
-      </div>
-    );
-  });
+  const tipBtns =
+  useMemo(()=>
+    tips.map((tip, index) => {
+      let tipNum = "tip" + (index + 1);
+      return (
+        <div key={index} className={`${tipNum} btn ${currentTip === index ? "choosed": ""}`} 
+        onClick={()=>{tipClickHandler(index)
+        }}
+        >
+          <label htmlFor={tipNum}>{tip}%</label>
+          <input type="radio" name="tip" value={tip} id={tipNum} hidden />
+        </div>
+      );
+    })
+  ,[currentTip])
+
 
   return (
     <section className="manage-area">
       <section className="bill">
         <div className="text">
           <label htmlFor="bill">Bill</label>
-          {bill == 0 && <span className="error">can't be zero</span>}
+          {bill === 0 && <span className="error">can't be zero</span>}
         </div>
 
         <div className="input">
-          <img src={dollar} alt="dollar-sign"/>
+          <img src={dollar} alt="dollar-sign" />
           <input
             id="bill"
             type="number"
             onChange={(e) => {
-              setCalcManager((prev)=>({...prev,bill:+e.target.value}))
-              setIsReseted(false)
-            }
-          }
-            value={bill == null ? "" : bill}
+              setCalcManager((prev) => ({ ...prev, bill: +e.target.value }));
+              setIsReseted(false);
+            }}
+            value={bill === null ? "" : bill}
             placeholder="0"
-            className={bill == 0 ? "error" : null}
+            className={bill === 0 ? "error" : null}
           />
         </div>
       </section>
@@ -96,33 +93,38 @@ function ManageArea({
         <form
           className="button-group"
           onChange={(e) => {
-            setCalcManager((prev)=>({...prev,tip:e.target.value}))
-            setIsReseted(false)
+            setCalcManager((prev) => ({ ...prev, tip: e.target.value }));
+            setIsReseted(false);
           }}
         >
           {tipBtns}
-          <div className="custom-btn btn" onClick={tipClickHandler}>
-            <input name="tip" placeholder="Custom" ref={CustomTip} />
+          <div className="custom-btn btn" 
+          >
+            <input name="tip" placeholder="Custom"
+            onChange={(e)=>customTipHandler(e)
+          }
+            value={customTip}
+            />
           </div>
         </form>
       </section>
       <section className="people">
         <div className="text">
           <label htmlFor="people">Number of People</label>
-          {people == 0 && <span className="error">can't be zero</span>}
+          {people === 0 && <span className="error">can't be zero</span>}
         </div>
         <div className="input">
           <img src={person} alt="people-sign" />
           <input
             id="people"
             type="number"
-            value={people == null ? "" : people}
+            value={people === null ? "" : people}
             placeholder="0"
             onChange={(e) => {
-              setCalcManager((prev)=>({...prev,people:e.target.value}))
-              setIsReseted(false)
+              setCalcManager((prev) => ({ ...prev, people: e.target.value }));
+              setIsReseted(false);
             }}
-            className={people == 0 ? "error" : null}
+            className={people === 0 ? "error" : null}
           />
         </div>
       </section>
--- src/Component/ResultArea.jsx
+++ src/Component/ResultArea.jsx
@@ -1,43 +1,38 @@
 import { useEffect, useState } from "react";
 
-function ResultArea({tipAmount,total,setIsReseted,isReseted}) {
+function ResultArea({ tipAmount, total, setIsReseted, isReseted }) {
   const [isDisabled, setIsDisabled] = useState(true);
-useEffect(()=>{
-  if(isReseted){
-    setIsDisabled(true)
-  }else setIsDisabled(false)
-},[isReseted])
-console.log(tipAmount)
-console.log(total)
+
+  useEffect(() => {
+    if (isReseted) {
+      setIsDisabled(true);
+    } else setIsDisabled(false);
+  }, [isReseted]);
+  
   return (
     <section className="result-area">
       <section className="tip-amount">
-          <div className="text">
-            <div className="head">Tip Amount</div>
-            <p>/ person</p>
-          </div>
-          <div className="tip-result">
-            ${tipAmount.toFixed(2)}
-            
-          </div>
+        <div className="text">
+          <div className="head">Tip Amount</div>
+          <p>/ person</p>
+        </div>
+        <div className="tip-result">${tipAmount.toFixed(2)}</div>
       </section>
       <section className="total">
-          <div className="text">
-            <div className="head">Total</div>
-            <p>/ person</p>
-          </div>
-          <div className="total-result">
-          ${total.toFixed(2)}
-          </div>
+        <div className="text">
+          <div className="head">Total</div>
+          <p>/ person</p>
+        </div>
+        <div className="total-result">${total.toFixed(2)}</div>
       </section>
-      <button  
-      onClick={()=>setIsReseted(true)}
-      className={isDisabled===true&&"disabled"}
+      <button
+        onClick={() => setIsReseted(true)}
+        className={isDisabled ? "disabled" : null}
       >
         Reset
       </button>
     </section>
-  )
+  );
 }
 
-export default ResultArea
+export default ResultArea;
